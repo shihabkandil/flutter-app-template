d5c7c25176001163b1ddc6fd989ef9ee9eccaae3
Shihab Kandil <shihab.mostafa1@gmail.com>
2024-07-17 18:52:08 +0300

    add development top-right banner on dev flavor

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'common/constants/constants.dart';
 import 'common/core/app_config.dart';
 import 'common/core/router/app_router.dart';
 import 'main.dart';
@@ -9,5 +10,16 @@ void main() async {
 
   commonMain();
 
-  runApp(App(appRouter: AppRouter()));
+  runApp(
+    Directionality(
+      textDirection: TextDirection.ltr,
+      child: Banner(
+        location: BannerLocation.topStart,
+        message: Constants.devFlavorAbbreviation,
+        child: App(
+          appRouter: AppRouter(),
+        ),
+      ),
+    ),
+  );
 }
